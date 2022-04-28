906176e9be5c9258efd9c7e01b077138bbee7be5
Thuan Hoang <thamthuy100272@gmail.com>
2022-04-28 23:59:20 +0700

    finish register

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnRegister;
+    private Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.button2);
+        btnRegister = findViewById(R.id.button);
+
+        btnSignIn.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+        });
+
+        btnRegister.setOnClickListener((view) -> {
+            Intent intent = new Intent(this, Register.class);
+            startActivity(intent);
+        });
+
+    }
+}
